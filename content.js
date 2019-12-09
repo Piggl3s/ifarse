fe87f83696dcabdcf7d90fe9cf766800e2ae7f9d
Nick Osborne <retrogamecavern@googlemail.com>
2019-12-09 20:00:48 +0000

    Update content.js

--- content.js
+++ content.js
@@ -1,63 +1,28 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
-  });
 
+let divs = document.getElementsByClassName('navbar-brand');
+for (let i = 0; i < divs.length; i++) {
+  divs[i].innerHTML = "ifarse";
+}
 
-//get todays date and break into d-m-y
-var todaynow = new Date();
-todaynow.setDate(todaynow.getDate());
-var dn = todaynow.getDate();
-var mn = todaynow.getMonth() + 1;
-var yn = todaynow.getFullYear();
-//get todays date and plus 7 days also breaking into d-m-y	
-var today = new Date();
-today.setDate(today.getDate() + 7);
-var d = today.getDate();
-var m = today.getMonth() +1;
-var y = today.getFullYear();
+const onReady = () => 
+  setTimeout(() => {
+    updateAttributes(8,1);
+  }, 60);
+onReady();
 
-if (m < 10) {
-    var ms = "0" + m;
-    }
-    else
-    var ms = m;
+const updateAttributes = (currentStatus, newStatus) => {
+  let arr = [...document.querySelectorAll(`[currparkingstatus="${currentStatus}"]`)];
+  if (arr.length > 0)
+    for (const x of arr)
+      x.setAttribute('currparkingstatus', newStatus);
+      chevronClick();
+  if (arr.length < 1)
+    onReady();
+}
 
-if (d< 10){
-    var ds = "0" + d;
-    }
-    else
-    var ds = d; 
-//if day less than 10 add 0 before  REMEMEBER single months require the 0 to be added - 1- and above do NOT!!
-var dateString = "calendar_" + y +"-" + ms +"-" + ds + "_day";
-
-//if the month of +7 days is the same as todays month do this   
-// if (mn == m) {
-//     //window.alert (dateString);
-//     setTimeout(function(){
-//         document.getElementById(dateString).click();
-//         }, 6000);
-        
-//         setTimeout(function(){
-//         document.getElementById("modalConfirmationSubmit").click();
-//         }, 10000);
-// }
-
-// //if the month of +7 days is in the next month do this              	
-// else {
-    
-//     setTimeout(function(){
-//         document.getElementById("calendar_nav-next").click();
-//         }, 6000);
-
-//     setTimeout(function(){
-        
-//         document.getElementById(dateString).click();
-//         //window.alert (mn);
-//        }, 9000);
-        
-//     setTimeout(function(){
-//         document.getElementById("modalConfirmationSubmit").click();
-//         }, 11000);
-
-// }
+function chevronClick(){
+  let chevright = document.getElementsByClassName('calendar-month-navigation');
+  for (let i = 0; i < chevright.length; i++) {
+    chevright[i].addEventListener ('click', onReady);
+  }
+}
